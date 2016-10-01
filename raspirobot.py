9cbfd3747139cd2e9507a4d7f3d31a5f8fb7045e
AssmannPhi <phill.trist@hotmail.de>
2016-10-01 11:21:29 +0200

    Update raspirobot.py

--- raspirobot.py
+++ raspirobot.py
@@ -71,7 +71,7 @@ while running:
                 
                         if Ausweichen == 1:
                                 rr.set_motors(0.25,0,0.25,1)
-                                time.sleep(1.2)
+                                time.sleep(0.7)
 
                         if Stehenbleiben == 1:
                                 rr.set_motors(0,0,0,0)
