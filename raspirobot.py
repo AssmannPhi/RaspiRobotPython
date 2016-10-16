af8985adde47764ea6021209f4e25e7b7034ee19
AssmannPhi <phill.trist@hotmail.de>
2016-10-16 11:28:44 +0200

    Update raspirobot.py

--- raspirobot.py
+++ raspirobot.py
@@ -87,12 +87,13 @@ while running:
                         rr.set_motors(0.25,0,0.25,0)
            
         if switch == True:
-                rr.set_led1(1)
-                rr.set_led2(1)
-                time.sleep(0.5)
-                rr.set_led1(0)
-                rr.set_led2(0)
                 os.system("sudo shutdown -h -t 5")
+                while True:
+                        rr.set_led1(1)
+                        rr.set_led2(1)
+                        time.sleep(0.5)
+                        rr.set_led1(0)
+                        rr.set_led2(0)
                 
                 
                 # Alle aufgelaufenen Events holen und abarbeiten.
