304cf5d37ffb6483ab99082166187362fe5fb939
AssmannPhi <phill.trist@hotmail.de>
2016-12-22 18:31:05 +0100

    Update raspirobot.py

--- raspirobot.py
+++ raspirobot.py
@@ -81,6 +81,16 @@ while running:
                                 time.sleep(0.5)
                                 rr.set_led1(0)
                                 rr.set_led2(0)
+                                
+                        if i < 35:
+                                
+                                rr.set_motors(0.25,1,0.25,1)
+                                rr.set_led1(1)
+                                rr.set_led2(1)
+                                time.sleep(0.05)
+                                rr.set_led1(0)
+                                rr.set_led2(0)
+
 
                 else:
                         
